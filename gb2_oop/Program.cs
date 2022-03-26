a46363c9482afe57f84460b6661150bf30fb7936
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-03-26 12:53:10 +0300

    completed tasks n2

--- gb2_oop/Program.cs
+++ gb2_oop/Program.cs
@@ -6,7 +6,55 @@ namespace gb2_oop
     {
         static void Main()
         {
-            
+            BankAccount account = new BankAccount();
+
+            account.UpdateNumber();
+            account.SetType(BankAccount.BankAccountType.Credit);
+            account.SetBalance(20000);
+
+            Console.WriteLine($"{account.GetNumber()} - {account.GetType()}, {account.GetBalance()} $");
+        }
+    }
+
+    class BankAccount
+    {
+        private static int UniqueNumber = 0;
+        private int Number = UniqueNumber;
+        private int Balance = 0;
+        private BankAccountType Type = BankAccountType.None;
+        public enum BankAccountType
+        {
+            None,
+            Current,
+            Settlement,
+            Credit
+        }
+
+        public int GetNumber()
+        {
+            return Number;
+        }
+        public void UpdateNumber()
+        {
+            UniqueNumber++;
+        }
+        
+        public long GetBalance()
+        {
+            return Balance;
+        }
+        public void SetBalance(int value)
+        {
+            Balance = value;
+        }
+        
+        public BankAccountType GetType()
+        {
+            return Type;
+        }
+        public void SetType(BankAccountType type)
+        {
+            Type = type;
         }
     }
 }
