4ac8c86db3aea812abeece77f7b36ea576ecae44
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-03-27 20:57:55 +0300

    completed task 3.2

--- gb2_oop/Program.cs
+++ gb2_oop/Program.cs
@@ -6,7 +6,21 @@ namespace gb2_oop
     {
         static void Main()
         {
-            
+            Console.WriteLine(ReverceString(String.Empty));
+            Console.WriteLine(ReverceString("This is a test"));
+            Console.WriteLine(ReverceString("AAppEEaa__^^"));
+        }
+
+        public static string ReverceString(string str)
+        {
+            char[] reversedAray = new char[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                reversedAray[i] = str[str.Length - i - 1];
+            }
+
+            return new string(reversedAray);
         }
     }
 }
