0c5aecbfda74634aa68765df875b1203561e43c0
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-03-27 21:34:35 +0300

    completed 3.3 task

--- gb2_oop/Program.cs
+++ gb2_oop/Program.cs
@@ -6,7 +6,19 @@ namespace gb2_oop
     {
         static void Main()
         {
+            string[] text = File.ReadAllLines("name_and_email.txt");
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                SearchMail(ref text[i]);
+            }
             
+            File.WriteAllLines("email.txt", text);
+        }
+
+        public static void SearchMail(ref string s)
+        {
+            s = s.Split('&')[1];
         }
     }
 }
